f595fe4219668e692c1850ac9dafe68b92d92962
HeSummer <18821755390@163.com>
2019-06-13 19:00:40 +0800

    增加商品POJO类

---
+++ HelloProjet/src/Items.java
@@ -0,0 +1,79 @@
+package com.aming.pojo;
+
+import java.io.Serializable;
+import java.util.Date;
+
+
+public class Items implements Serializable {
+	private Integer id;
+	private String name;
+	private double price;
+	private String pic;
+	private Date createtime;
+	private String detail;
+
+	public static long getSerialVersionUID() {
+		return serialVersionUID;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public String getPic() {
+		return pic;
+	}
+
+	public void setPic(String pic) {
+		this.pic = pic;
+	}
+
+	public Date getCreatetime() {
+		return createtime;
+	}
+
+	public void setCreatetime(Date createtime) {
+		this.createtime = createtime;
+	}
+
+	public String getDetail() {
+		return detail;
+	}
+
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+
+	@Override
+	public String toString() {
+		return "Items{" +
+				"id=" + id +
+				", name='" + name + '\'' +
+				", price=" + price +
+				", pic='" + pic + '\'' +
+				", createtime=" + createtime +
+				", detail='" + detail + '\'' +
+				'}';
+	}
+}
+
